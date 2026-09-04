26a7b46e1a3f20a9671a3ef627b4c1524e12824f
Nikolai Grlica <nikolai@nikolaigrlica.dev>
2026-09-04 19:14:20 +0200

    feat(tauri): enforce single-instance app focus
    Signed-off-by: Nikolai Grlica <nikolai@nikolaigrlica.dev>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,7 +22,19 @@ use state::AppState;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(desktop)]
+    {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            let _ = app
+                .get_webview_window("main")
+                .expect("no main window")
+                .set_focus();
+        }));
+    }
+
+    builder
         .plugin(tauri_plugin_clipboard_manager::init())
         .setup(|app| {
             let icon =
